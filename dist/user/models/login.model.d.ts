c8df45b77342c92c077129ca258ffe5300207a4c
villanovadev <villanovadev@gmail.com>
2018-10-14 03:26:27 -0500

    deleted

--- dist/user/models/login.model.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class LoginModel {
-    email: string;
-    password: string;
-}
--- dist/user/models/register.model.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class RegisterModel {
-    email: string;
-    password: string;
-    isHelper: boolean;
-}
